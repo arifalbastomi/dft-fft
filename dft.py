1cd2bd1f65b9cd7b27e3236b10acc8d8102ba843
arif.albastomi <tomyysantoso23@gmail.com>
2020-08-23 17:22:02 +0700

    dft-fft

---
+++ dft.py
@@ -0,0 +1,44 @@
+import numpy as np
+from datetime import datetime
+
+def DFT_slow(x):
+
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+def FFT(x):
+
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return DFT_slow(x)
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+        return np.concatenate([X_even + factor[:N / 2] * X_odd,
+                               X_even + factor[N / 2:] * X_odd])
+
+#random
+
+x = np.random.random(10024)
+start = datetime.now()
+print(start)
+np.allclose(DFT_slow(x), np.fft.fft(x))
+print(x)
+print(datetime.now())
+
+
+
+
+
+
+
+
